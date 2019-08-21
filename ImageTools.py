2b2d431cbb5a60742362a146592bce6744ba0493
RobZinke <zink.oxide81@gmail.com>
2019-08-21 08:31:53 -0700

    Major changes: Removed fitPlane from ImageTools; Added new separate definition for creat vector fields (2+ band imagery)

--- ImageTools.py
+++ ImageTools.py
@@ -19,6 +19,7 @@ from mpl_toolkits import mplot3d
 from scipy import signal as sig 
 from scipy import interpolate as intp 
 from scipy import integrate 
+from fitPlane import *
 
 # --- Formulae for later --- 
 def gauss(x,mu,sig):
@@ -435,135 +436,6 @@ def intCorr(I1,I2,window=None,vocal=False,plot=False):
 		cax1=ax1.imshow(C,cmap='cividis') 
 		F.colorbar(cax1,orientation='horizontal') 
 	return xshift, yshift 
-
-# --- Fit plane --- 
-# Fit a 2D plane to data 
-class fitPlane:
-	def __init__(self,data,dtype,ds=0,vocal=False,plot=False): 
-		# Fit a plane to the points 
-		# INPUTS 
-		#	data is the dataset, either as: 
-		#	  m x 3 points (x,y,z) 
-		#	  m x n 2D array with no spatial reference 
-		#	dtype is the data type: 
-		#	  'points' or 'pts' 
-		#	  'image', 'img', or 'map' 
-		#	ds is the downsample factor (power of 2) 
-		#	vocal = True/False 
-		#	plot  = True/False 
-		# OUTPUTS 
-		#	V are the first two right singular vectors 
-		#	N is the unit normal to the plane 
-
-		if vocal is True: 
-			print('Fitting plane to %s' % (dtype)) 
-			print('\tdata dimensions:',data.shape) 
-		# Center and format  
-		if dtype is 'points' or dtype is 'pts': 
-			# Full x,y,z coordinates specified 
-			nData=data.shape[0] # number of data points 
-			xcenter=np.mean(data[:,0]) 
-			ycenter=np.mean(data[:,1]) 
-			zcenter=np.mean(data[:,2]) 
-			data[:,0]=data[:,0]-xcenter # remove centroid 
-			data[:,1]=data[:,1]-ycenter 
-			data[:,2]=data[:,2]-zcenter 
-			xMin=data[:,0].min(); xMax=data[:,0].max() 
-			yMin=data[:,1].min(); yMax=data[:,1].max() 
-			zMin=data[:,2].min(); zMax=data[:,2].max() 
-			x=np.linspace(xMin,xMax,5); y=np.linspace(yMin,yMax,5) 
-			X,Y=np.meshgrid(x,y) # grid surface 
-			P=data # assign directly to points array 
-			# Downsample 
-			ds=int(2**ds) # downsample factor 
-			P=P[::ds,:] # downsample data 
-			nData=P.shape[0] # final number of data points 
-		elif dtype is 'image' or dtype is 'img' or dtype is 'map': 
-			# Map-like 2D array - create mapping coordinates 
-			m,n=data.shape # original shape 
-			nData=m*n # total number of data points 
-			xcenter=0.; ycenter=0. 
-			zcenter=np.mean(data); data=data-zcenter # remove centroid 
-			xMin=-np.floor(n/2); xMax=np.ceil(n/2) 
-			yMin=-np.floor(m/2); yMax=np.ceil(m/2) 
-			zMin=data.min(); zMax=data.max() 
-			x=np.linspace(xMin,xMax,n); y=np.linspace(yMin,yMax,m) 
-			X,Y=np.meshgrid(x,y) # grid surface 
-			# Format into [x,y,z] array 
-			P=np.hstack([X.reshape(nData,1),Y.reshape(nData,1),data.reshape(nData,1)]) 
-			# Downsample 
-			ds=int(2**ds) # downsample factor 
-			P=P[::ds,:] # downsample data 
-			nData=P.shape[0] # final number of data points 
-		if vocal is True: 
-			print('\tCenter (%f, %f, %f)' % (xcenter,ycenter,zcenter)) 
-			print('\tBounds\n\t\tx: %f, %f\n\t\ty: %f, %f\n\t\tz: %f, %f' % 
-				(xMin,xMax,yMin,yMax,zMin,zMax)) 
-			print('\tP size:',P.shape) 
-		# Decompose using SVD
-		U,s,VT=np.linalg.svd(P) 
-		S=np.zeros((nData,3)); S[:3,:3]=np.diag(s) 
-		# Reduce to 2D
-		S[2,2]=0. # null third dimension 
-		Q=U.dot(S.dot(VT)) # project onto plane 
-		# Vectors 
-		V=VT[:2,:].T # first two right singular vectors 
-		dotV=np.dot(V[:,0],V[:,1]) # dot product to confirm orthonormality 
-		N=np.cross(V[:,0],V[:,1]) # normal to plane 
-		N=N/np.linalg.norm(N) # confirm unit length 
-		if vocal is True: 
-			print('\tSingular vectors:') 
-			print('\t\tV1: ', V[:,0].T) 
-			print('\t\tV2: ', V[:,1].T) 
-			print('\t\t\tdot: %f' % (dotV)) 
-			print('\tNormal to plane:\n\t\tN:', N.T) 
-			print('\t\t\tdot: %f; %f' % (np.dot(N,V[:,0]),np.dot(N,V[:,1]))) 
-		# Plane 
-		Z=-1/N[2]*((N[0]*X)+(N[1]*Y)) 
-		# Plot if specified 
-		if plot is not False: 
-			if dtype is 'points' or dtype is 'pts': 
-				# Plot data as points 
-				F=plt.figure() 
-				ax=F.add_subplot(111,projection='3d') 
-				ax.plot_surface(X,Y,Z,color='b',alpha=0.25,zorder=1) 
-				ax.quiver(0,0,0,V[0,0],V[1,0],V[2,0],color='g') 
-				ax.quiver(0,0,0,V[0,1],V[1,1],V[2,1],color='r') 
-				ax.quiver(0,0,0,N[0],N[1],N[2],color='k')
-				ax.plot(Q[:,0],Q[:,1],Q[:,2],'b.',zorder=3)
-				for i in range(nData): 
-					ax.plot([Q[i,0],P[i,0]],[Q[i,1],P[i,1]],[Q[i,2],P[i,2]],'b')
-				ax.plot(P[:,0],P[:,1],P[:,2],'ko',zorder=3) 
-				ax.set_xlim([xMin,xMax]); ax.set_xlabel('-x-') 
-				ax.set_ylim([yMin,yMax]); ax.set_ylabel('-y-') 
-				ax.set_zlim([zMin,zMax]); ax.set_zlabel('-z-') 
-				ax.set_aspect(1) 
-				ax.set_title('SVD plane fit (centered)')
-			elif dtype is 'image' or dtype is 'img' or dtype is 'map': 
-				# Plot data as image 
-				if type(plot) is str:
-					cmap=plot 
-				else: 
-					cmap='viridis' 
-				F=plt.figure() 
-				if m>n: 
-					Prows=1; Pcols=2 
-				else: 
-					Prows=2; Pcols=1 
-				extent=(xMin,xMax,yMin,yMax) 
-				ax1=F.add_subplot(Prows,Pcols,1) 
-				cax1=ax1.imshow(data,cmap=cmap,extent=extent)
-				ax1.set_title('Data (centered)') 
-				F.colorbar(cax1,orientation='vertical') 
-				ax2=F.add_subplot(Prows,Pcols,2) 
-				cax2=ax2.imshow(Z,extent=extent) 
-				ax2.set_title('SVD plane fit') 
-				F.colorbar(cax2,orientation='vertical') 
-		# Outputs 
-		self.V=V # singular vectors 
-		self.N=N # normal to plane 
-		self.Q=Q # points on plane 
-		self.Z=Z # hypothetical plane 
 
 
 ##################################
---
+++ vectorField.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from osgeo import gdal
+
+class vField:
+	'''
+	EW is the east-west or x field
+	NS is the north-south or y field
+	xWin is the window size (px) in x - must be odd
+	yWin is the window size (px) in y - must be odd
+	xstep is the step size (px) in the x direction - must be even
+	ystep is the step size (px) in the y direction - must be even
+	'''
+
+	# --- Create vectors ---
+	def __init__(self,EW,NS,xWin,yWin,xstep,ystep,geoTransform=None,vocal=False):
+		assert EW.shape==NS.shape, print('Images must be same size')	
+
+		# Image/spatial parameters
+		nImg=NS.shape[1] # image dimension in x
+		mImg=EW.shape[0] # image dimension in y
+
+		# Parse geo transform if specified
+		if geoTransform is not None:
+			left=geoTransform[0] # upper left x-coordinate
+			top=geoTransform[3] # upper left y-coordinate
+			dx=geoTransform[1] # pixel size in x
+			dy=geoTransform[5] # pixel size in y
+			right=left+nImg*dx # easternmost extent
+			bottom=top+mImg*dy # southernmost extent
+			self.extent=(left, right, bottom, top)
+		else:
+			ULx=0; ULy=0
+			dx=1; dy=1
+			self.extent=(0, nImg, 0, mImg)
+
+
+		# Estimate number of steps based on pixel size
+		xWinWidth=int(xWin/2) # half-window width in x
+		yWinWidth=int(yWin/2) # half-window width in y
+
+		n_y_steps=int(mImg/ystep) # number of steps in y
+		m_x_steps=int(nImg/xstep) # number of steps in x
+		self.total_points=n_y_steps*m_x_steps # total number of measurements
+
+		y2step=int(ystep/2) # half step in y
+		x2step=int(xstep/2) # half step in x
+
+		if vocal is True:
+			print('\tSteps in x: %i' % m_x_steps)
+			print('\tSteps in y: %i' % n_y_steps)
+
+		xlocations=np.arange(x2step,nImg,xstep)
+		ylocations=np.arange(y2step,mImg,ystep)
+
+		# Determine vectors
+		self.x=np.zeros((self.total_points))
+		self.y=np.zeros((self.total_points))
+		self.u=np.zeros((self.total_points))
+		self.v=np.zeros((self.total_points))
+
+		k=0
+		for i in range(n_y_steps):
+			for j in range(m_x_steps):
+				self.x[k]=xlocations[j]
+				self.y[k]=ylocations[i]
+
+				x_region=np.arange(xlocations[j]-xWinWidth,xlocations[j]+xWinWidth+1)
+				y_region=np.arange(ylocations[i]-yWinWidth,ylocations[i]+yWinWidth+1)
+				
+				self.u[k]=np.nanmean(EW[y_region,x_region])
+				self.v[k]=np.nanmean(NS[y_region,x_region])
+
+				k+=1
+
+		self.x=self.x*dx+left
+		self.y=self.y*dy+top
+
+
+	# --- Plot on image of choice ---
+	def plotVectors(self,img,arrow_color='k',cmap='viridis',headwidth=3,headlength=5):
+		F=plt.figure()
+		ax=F.add_subplot(111)
+		cax=ax.imshow(img,cmap=cmap,extent=self.extent,zorder=1)
+		ax.quiver(self.x,self.y,self.u,self.v,
+			units='xy',color=arrow_color,
+			headwidth=headwidth,headlength=headlength,
+			zorder=2)
+		F.colorbar(cax,orientation='vertical')
+		ax.set_aspect(1)
